a3a54ce33d93df763801c465ab84b1e03c37c1e3
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-27 20:48:28 +0100

    :tada: Start new Typescript project

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <template>
-  <v-app>
-    <Nav />
-    <v-content>
-      <router-view :key="$route.path" />
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<script>
-import Vue from "vue";
-import axios from "axios";
-import Nav from "@/components/Nav.vue";
-import { mapGetters } from "vuex";
-export default {
-  name: "App",
-  computed: {
-    ...mapGetters(["auth"])
-  },
-  components: {
-    Nav
-  },
-  mounted() {
-    if (this.auth && this.auth.token) {
-      const instance = axios.create({
-        baseURL: "https://developer.oswaldlabs.com/",
-        headers: { Authorization: "Bearer " + this.auth.token }
-      });
-      Vue.prototype.$http = instance;
-    }
-  },
-  watch: {
-    auth() {
-      if (this.auth && this.auth.token) {
-        const instance = axios.create({
-          baseURL: "https://developer.oswaldlabs.com/",
-          headers: { Authorization: "Bearer " + this.auth.token }
-        });
-        Vue.prototype.$http = instance;
-      }
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
-};
-</script>
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,128 +1,17 @@
 <template>
-  <v-card flat>
-    <v-toolbar color="primary" dark extended flat>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-    </v-toolbar>
-    <v-layout row pb-2>
-      <v-flex xs4 offset-xs4>
-        <v-card class="card--flex-toolbar">
-          <v-toolbar card prominent>
-            <v-toolbar-title class="body-2 grey--text">Login</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>more_vert</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-divider></v-divider>
-          <v-card-text>
-            <v-snackbar v-model="hasError" :timeout="5000" bottom right>
-              {{ error }}
-              <v-btn
-                color="pink"
-                flat
-                @click="
-                  hasError = false;
-                  error = '';
-                "
-                >Close</v-btn
-              >
-            </v-snackbar>
-            <v-form class="form" @submit.prevent="login">
-              <v-text-field
-                v-model="email"
-                type="email"
-                label="Email"
-                required
-              />
-              <v-text-field
-                v-model="password"
-                type="password"
-                label="Password"
-                required
-              />
-              <v-btn
-                :loading="loading"
-                :disabled="loading"
-                color="blue"
-                class="white--text button-2"
-                type="submit"
-                block
-              >
-                Log in with Oswald Labs
-              </v-btn>
-              <v-btn class="button-2" block to="/register"
-                >Sign up for a new account</v-btn
-              >
-            </v-form>
-          </v-card-text>
-        </v-card>
-        <div style="padding: 2rem; text-align: center">
-          <router-link to="/forgot">Forgot your password?</router-link>
-        </div>
-      </v-flex>
-    </v-layout>
-  </v-card>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      hasError: false,
-      error: "",
-      loading: false,
-      email: "",
-      password: ""
-    };
-  },
-  watch: {
-    error() {
-      this.hasError = !!this.error;
-    }
-  },
-  mounted() {
-    if (this.$store.state.auth && this.$store.state.auth.token)
-      this.$router.push("/my-apis");
-  },
-  methods: {
-    login() {
-      this.loading = true;
-      this.$http
-        .post("/auth/login", {
-          email: this.email,
-          password: this.password
-        })
-        .then(response => {
-          this.$store.commit("updateAuth", response.data);
-          this.$router.push("/my-apis");
-        })
-        .catch(error => {
-          if (
-            error.response &&
-            error.response.data &&
-            error.response.data.error
-          ) {
-            this.error = error.response.data.error;
-          }
-        })
-        .then(() => {
-          this.loading = false;
-          this.email = "";
-          this.password = "";
-        });
-    }
-  }
-};
-</script>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-<style>
-.card--flex-toolbar {
-  margin-top: -64px;
-}
-.form {
-  padding: 2rem;
-}
-.button-2 {
-  margin-top: 1.5rem;
-}
-</style>
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
