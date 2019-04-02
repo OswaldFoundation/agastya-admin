be7a6f53c982fde56c7b579fc0e0a3f81e1e4e1d
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-02 10:19:32 +0200

    Remove Sentry in favor of Agastya integration

--- src/main.js
+++ src/main.js
@@ -6,11 +6,7 @@ import router from "./router";
 import store from "./store";
 import iconify from "./iconify";
 import VueHighlightJS from "vue-highlightjs";
-import * as Sentry from "@sentry/browser";
 import "./registerServiceWorker";
-Sentry.init({
-  dsn: "https://f42354e3dbdc4cf091ea761f2ed52ed4@sentry.io/1427894"
-});
 
 Vue.config.productionTip = false;
 const instance = axios.create({
